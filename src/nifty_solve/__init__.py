bdc67fd0ed47636babc2722ae0a6b983f11f6966
Tom Hilder <tom.hilder.dlhp@gmail.com>
2024-12-13 18:10:29 +1100

    add code from prototyping notebook for jax version of operators

--- src/nifty_solve/__init__.py
+++ src/nifty_solve/__init__.py
@@ -1,1 +1,10 @@
-from .operators import Finufft1DRealOperator, Finufft2DRealOperator, Finufft3DRealOperator
+from .jax_operators import (
+    JaxFinufft1DRealOperator,
+    JaxFinufft2DRealOperator,
+    JaxFinufft3DRealOperator,
+)
+from .operators import (
+    Finufft1DRealOperator,
+    Finufft2DRealOperator,
+    Finufft3DRealOperator,
+)
