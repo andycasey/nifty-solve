131b274f0c9ae5e4bdff8a7610291fc95c1a2e93
Andy Casey <andycasey@gmail.com>
2024-12-13 08:39:41 -0500

    no full jax imports to prevent breaking situations when jax is not installed

--- src/nifty_solve/__init__.py
+++ src/nifty_solve/__init__.py
@@ -1,8 +1,13 @@
-from .jax_operators import (
-    JaxFinufft1DRealOperator,
-    JaxFinufft2DRealOperator,
-    JaxFinufft3DRealOperator,
-)
+try:
+    from .jax_operators import (
+        JaxFinufft1DRealOperator,
+        JaxFinufft2DRealOperator,
+        JaxFinufft3DRealOperator,
+    )
+except ImportError:
+    # No JAX option installed. Maybe we should create dummy classes and raise NotAvailableError on init?
+    None
+
 from .operators import (
     Finufft1DRealOperator,
     Finufft2DRealOperator,
