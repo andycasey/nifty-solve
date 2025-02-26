6abeeb30866fcac58974249a4dc65a354cdc82b9
Andy Casey <andycasey@gmail.com>
2025-02-26 15:50:15 +1100

    ignore jax operators for now

--- src/nifty_solve/__init__.py
+++ src/nifty_solve/__init__.py
@@ -1,2 +1,2 @@
 from .operators import (Finufft1DRealOperator, Finufft2DRealOperator, Finufft3DRealOperator)
-from .jax_operators import (JaxFinufft1DRealOperator, JaxFinufft2DRealOperator, JaxFinufft3DRealOperator)
+#from .jax_operators import (JaxFinufft1DRealOperator, JaxFinufft2DRealOperator, JaxFinufft3DRealOperator)
