4587d0f2aed1badbf092834bd496f03515f9c645
Andy Casey <andycasey@gmail.com>
2025-03-26 16:23:19 +1100

    separate complex / real finufft operators

--- src/nifty_solve/operators.py
+++ src/nifty_solve/operators.py
@@ -7,7 +7,7 @@ from typing import Optional, Union
 
 from .utils import odd_ceiling, expand_to_dim
 
-class FinufftRealOperator(LinearOperator):
+class FinufftOperator(LinearOperator):
     def __init__(self, *points, n_modes: Union[int, tuple[int]], **kwargs):
         if len(set(map(len, points))) != 1:
             raise ValueError("All point arrays must have the same length.")
@@ -20,7 +20,7 @@ class FinufftRealOperator(LinearOperator):
         # This is the requested number of modes, but may not be the actual.
         n_requested_modes = expand_to_dim(n_modes, len(points))
         super().__init__(
-            dtype=self.DTYPE_REAL,
+            dtype=self.DTYPE_COMPLEX,
             shape=(len(points[0]), int(np.prod(n_requested_modes))),
         )
         self.explicit = False
@@ -40,6 +40,19 @@ class FinufftRealOperator(LinearOperator):
         self._plan_rmatvec = finufft.Plan(1, **self.finufft_kwds)
         self._plan_matvec.setpts(*points)
         self._plan_rmatvec.setpts(*points)
+
+    def _matvec(self, c):
+        return self._plan_matvec.execute(c)
+
+    def _rmatvec(self, f):
+        return self._plan_rmatvec.execute(f.astype(self.DTYPE_COMPLEX))
+
+class FinufftRealOperator(FinufftOperator):
+
+    def __init__(self, *points, n_modes: Union[int, tuple[int]], **kwargs):
+        super().__init__(*points, n_modes=n_modes, **kwargs)
+        self.dtype = self.DTYPE_REAL
+        return None
 
     def _pre_matvec(self, c):
         m, h, p = self._shape_half_p
